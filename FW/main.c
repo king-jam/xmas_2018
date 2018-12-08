37327171f1f826a142a1ac9ef0ecbe4b0fd58bda
James King <james.king@dell.com>
2018-12-08 10:54:43 -0500

    Code comments and cleanup

--- FW/main.c
+++ FW/main.c
@@ -5,7 +5,7 @@
 #include <avr/sleep.h>
 
 #define F_CPU 1000000UL
-#define COMPA_INT 99 // every 1us with a prescalar of 1 and clock of 1MHz for 1ms
+#define COMPA_INT 99 // every 1us with a prescalar of 1 and clock of 1MHz
 #define LEDS 20
 #define PROGRAM_TRANSITION_DELAY_MS 1000
 
@@ -13,13 +13,15 @@
 extern const unsigned char led_port_dir[20] PROGMEM;
 extern const unsigned char led_port_lvl[20] PROGMEM;
 
-//unsigned char active_leds[4] = {0,1,2,3};
 unsigned int millis = PROGRAM_TRANSITION_DELAY_MS;
+unsigned char pwmMax = 100;
+// lone star led state pin
 unsigned char pinState = 0;
 unsigned int tickCount = 0;
 unsigned int pwmValue = 0;
-unsigned char pwmMax = 100;
+// increment to fade (increase for faster transition)
 unsigned char fadeIncrement = 1;
+// state to swap and up/down effect of fading
 unsigned char up = 1;
 
 void inline config_power_savings() {
@@ -40,22 +42,34 @@ void inline setup_timer0() {
 	// Enable CTC interrupt
 	TIMSK0 |= ( 1 << OCIE0A );
 	
-	// set to the compare interval (1ms)
+	// Set to the compare interval
 	OCR0A = COMPA_INT;
 }
 
 void leds_off() {
+	// set the port directions to input
 	DDRA = 0;
+
+	// turn of all the port pins
 	PORTA = 0;
+
+	// set the port directions to input
 	DDRB = 0;
+
+	// turn of all the port pins
 	PORTB = 0;
 }
 
 void light_led(unsigned char led) {
+	// declare variables in RAM to read from PROGMEM
 	unsigned char dir = 0;
 	unsigned char lvl = 0;
+
+	// do the PROGMEM read into those variables
 	dir = pgm_read_byte(&(led_port_dir[led]));
 	lvl = pgm_read_byte(&(led_port_lvl[led]));
+
+	// set the direction of everything per the port_map.h defs
 	DDRA = (dir & PA_MASK) >> PA_RS;
 	PORTA = (lvl & PA_MASK) >> PA_RS;
 	DDRB = (dir & PB_MASK) >> PB_RS;
@@ -68,7 +82,6 @@ void handle_pwm() {
     // determine if we're counting on or off time
   if (pinState == 1) {
     // see if we hit the desired on percentage
-    // not as precise as 255 or 1024, but easier to do math
     if (tickCount >= pwmValue) {
       pinState = 0;
     }
@@ -79,8 +92,6 @@ void handle_pwm() {
       tickCount = 0;
     }
   }
-  // could probably use some bitwise optimization here, digitalWrite()
-  // really slows things down after 10 pins.
   if(pinState == 1) {
   	light_led(6);
   } else {
@@ -88,9 +99,7 @@ void handle_pwm() {
   }
 }
 
-// Handle state redraw timer interval
-// This handles redrawing all the LEDs
-// so often to ensure only one is lit at a time
+// Handle state change timer interval
 ISR(TIM0_COMPA_vect) {
 	handle_pwm();
 
@@ -119,6 +128,8 @@ int main(void) {
 	setup_timer0();
 	sei();
 	while(1) {
+		// go to sleep when doing nothing
+		// interrupts are our friend
 		set_sleep_mode(SLEEP_MODE_IDLE);
 		cli();
 		sleep_enable();
