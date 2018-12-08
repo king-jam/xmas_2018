819f0c33c851ab853b4c84bc94a1d6e5b0030470
James King <james.king@dell.com>
2018-12-08 10:46:23 -0500

    Moved to one nice LED since HW issues

--- FW/main.c
+++ FW/main.c
@@ -6,25 +6,22 @@
 #include <avr/sleep.h>
 
 #define F_CPU 1000000UL
-#define COMPA_INT 999 // every 1ms with a prescalar of 1 and clock of 1MHz for 1ms
+#define COMPA_INT 99 // every 1us with a prescalar of 1 and clock of 1MHz for 1ms
 #define LEDS 20
-#define PROGRAM_TRANSITION_DELAY_MS 200
+#define PROGRAM_TRANSITION_DELAY_MS 1000
 
 // pull in the port maps in port_map.h via the extern
 extern const unsigned char led_port_dir[20] PROGMEM;
 extern const unsigned char led_port_lvl[20] PROGMEM;
-extern void *memcpy_P(void *, const void *, size_t);
 
-unsigned char led_states[20] = { 0 };
-unsigned char led_states_idx = 0;
-unsigned char millis = PROGRAM_TRANSITION_DELAY_MS; // set to trigger on first match
-unsigned char program_idx = 0;
-
-const led_prog_record program[3] PROGMEM = {
-	{{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1}},
-	{{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0}},
-	{{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1}},
-};
+//unsigned char active_leds[4] = {0,1,2,3};
+unsigned int millis = PROGRAM_TRANSITION_DELAY_MS;
+unsigned char pinState = 0;
+unsigned int tickCount = 0;
+unsigned int pwmValue = 0;
+unsigned char pwmMax = 100;
+unsigned char fadeIncrement = 1;
+unsigned char up = 1;
 
 void inline config_power_savings() {
 	// we will want to disable modules that aren't needed
@@ -48,6 +45,13 @@ void inline setup_timer0() {
 	OCR0A = COMPA_INT;
 }
 
+void leds_off() {
+	DDRA = 0;
+	PORTA = 0;
+	DDRB = 0;
+	PORTB = 0;
+}
+
 void light_led(unsigned char led) {
 	unsigned char dir = 0;
 	unsigned char lvl = 0;
@@ -59,53 +63,70 @@ void light_led(unsigned char led) {
 	PORTB = (lvl & PB_MASK) >> PB_RS;
 }
 
-void inline leds_off() {
-	DDRA = 0;
-	PORTA = 0;
-	DDRB = 0;
-	PORTB = 0;
-}
+// void refresh_leds() {
+// 	unsigned char led_states_idx = last_on_idx + 1;
+// 	leds_off();
+// 	while(led_states_idx != last_on_idx) {
+// 		if(led_states[led_states_idx] == 1) {
+// 			light_led(led_states_idx);
+// 			last_on_idx = led_states_idx;
+// 			break;
+// 		}
+// 		led_states_idx++;
+// 		if(led_states_idx == LEDS) {
+// 			led_states_idx = 0;
+// 		}
+// 	}
+// }
 
-void inline refresh_leds() {
-	for(unsigned char i = led_states_idx; i < LEDS; i++) {
-		if(led_states[i] == 1) {
-			leds_off();
-			light_led(i);
-			led_states_idx = i + 1;
-			break;
-		}
-	}
-	if(led_states_idx == LEDS) {
-		led_states_idx = 0;
-	}
-}
-
-void inline update_led_states_from_program() {
-	// do update
-	led_prog_record record;
-	memcpy_P(&record, &(program[program_idx]), sizeof(record));
-	for(unsigned char i = 0; i < LEDS; i++) {
-		led_states[i] = (record.all & (1 << i));
-	}
-	// bump idx
-	program_idx++;
-	// if we are at the number of rows, we are too far and need 
-	// to reset
-	if(program_idx == sizeof(program)/sizeof(program[0])) {
-		program_idx = 0;
-	}
+void handle_pwm() {
+  tickCount++;
+    // determine if we're counting on or off time
+  if (pinState == 1) {
+    // see if we hit the desired on percentage
+    // not as precise as 255 or 1024, but easier to do math
+    if (tickCount >= pwmValue) {
+      pinState = 0;
+    }
+  } else {
+    // if it isn't on, it is off
+    if (tickCount >= pwmMax) {
+      pinState = 1;
+      tickCount = 0;
+    }
+  }
+  // could probably use some bitwise optimization here, digitalWrite()
+  // really slows things down after 10 pins.
+  if(pinState == 1) {
+  	light_led(6);
+  } else {
+  	leds_off();
+  }
 }
 
 // Handle state redraw timer interval
 // This handles redrawing all the LEDs
 // so often to ensure only one is lit at a time
 ISR(TIM0_COMPA_vect) {
+	handle_pwm();
+
 	if(millis == PROGRAM_TRANSITION_DELAY_MS) {
 		millis = 0;
-		update_led_states_from_program();
+		if(up) {
+			pwmValue += fadeIncrement;
+			if(pwmValue > 30) {
+				pwmValue = 30;
+				up = 0;
+			}
+		} else {
+			pwmValue -= fadeIncrement;
+			if(pwmValue <= 0) {
+				pwmValue = 0;
+				up = 1;
+			}
+		}
 	}
 	millis++;
-	refresh_leds();
 }
 
 // Define the main function
@@ -113,7 +134,6 @@ int main(void) {
 	config_power_savings();
 	setup_timer0();
 	sei();
-	DDRA = ( 1 << DDRA0 );
 	while(1) {
 		set_sleep_mode(SLEEP_MODE_IDLE);
 		cli();
